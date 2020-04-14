6eb6ac6a237e7f5dfd8aa30c71277cca8ff4b912
midzer <midzer@gmail.com>
2020-04-14 13:01:12 +0200

    try to fix keyboard not showing up on mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -486,7 +486,7 @@ const citiesChoices  = new Choices(document.getElementById('cities'), {
   });
 })
 .then(function(instance) {
-  instance.containerOuter.element.addEventListener('focus', function() {
+  instance.containerOuter.element.addEventListener('click', function() {
     // Load data after user input
     instance.setChoices(async function() {
       const regions = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];

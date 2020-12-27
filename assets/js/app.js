41dae0412cc0baa4a7ca3e110e440b12c17e2032
midzer <midzer@gmail.com>
2020-12-27 01:08:14 +0100

    introduce shoutbox

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,9 @@ let vectorLayer;
 let citiesChoices;
 let shopsChoices;
 
+// Constants
+const SUBDOMAIN = window.location.hostname.split(".")[0];
+
 // Helpers
 // adopted from https://github.com/gabmontes/fast-haversine
 const R = 6378;
@@ -456,7 +459,7 @@ window.addEventListener('DOMContentLoaded', function() {
 });
 // Search
 async function fetchRegions() {
-  const response = await fetch('/' + window.location.hostname.split(".")[0] + '.json');
+  const response = await fetch('/' + SUBDOMAIN + '.json');
   const regions = await response.json();
   return regions;
 }
@@ -591,4 +594,125 @@ if (shops) {
     searchResultLimit: 100,
     position: 'bottom'
   }).disable();
+}
+// Chat
+function removeTags (string) {
+  return string.replace(/<(?:.|\n)*?>/gm, '')
+}
+
+function md2html (md) {
+  const bold_pattern1 = /\*{2}(.+)\*{2}/gim // <b>
+  const bold_pattern2 = /\_{2}(.+)\_{2}/gim  // <b>
+  const italic_pattern1 = /\_(.+)\_/gim // <i>
+  const italic_pattern2 = /\*(.+)\*/gim // <i>
+  const striketrough_pattern = /\~{2}(.+)\~{2}/gim // <del>
+  const a_pattern1 = /\[(.+)\]\((.+)\)/gim // <a>
+  const a_pattern2 = /\[(.+)\]\((.+) \"(.+)\"\)/gim // <a>
+  const a_pattern3 = /\[(.+)\]/gim // <a>
+
+  /* links */
+  md = md.replace(a_pattern1, function(match, title, url) {
+    return '<a href="' + url + '">' + title + '</a>'
+  })
+  md = md.replace(a_pattern2, function(match, title, url, tooltip) {
+    return '<a href="' + url + '" title="' + tooltip + '">' + title + '</a>'
+  })
+  md = md.replace(a_pattern3, function(match, url) {
+    return '<a href="' + url + '">' + url + '</a>'
+  })
+
+  /* bold */
+  md = md.replace(bold_pattern1, function(match, str) {
+    return '<b>' + str + '</b>'
+  })
+  md = md.replace(bold_pattern2, function(match, str) {
+    return '<b>' + str + '</b>'
+  })
+
+  /* italic */
+  md = md.replace(italic_pattern1, function(match, str) {
+    return '<i>' + str + '</i>';
+  })
+  md = md.replace(italic_pattern2, function(match, str) {
+    return '<i>' + str + '</i>';
+  })
+
+  /* striketrough */
+  md = md.replace(striketrough_pattern, function(match, str) {
+    return '<del>' + str + '</del>'
+  })
+
+  return md
+}
+
+function createMessage (content) {
+  const item = document.createElement('li')
+  item.innerHTML = md2html(content)
+
+  return item
+}
+
+function appendToList (child) {
+  chatbox.appendChild(child)
+  chatbox.scrollTop = chatbox.scrollHeight
+}
+
+function sendMessage (message) {
+  appendToList(createMessage(message))
+  ws.send(message)
+}
+
+function sendTextMessage () {
+  if (chatInput.value) {
+    sendMessage(removeTags(chatInput.value))
+    chatInput.value = ''
+    updateNumberMessages(1)
+  }
+}
+
+function updateNumberMessages(length) {
+  numberMessages += length;
+  chatSamp.textContent = numberMessages;
+}
+
+const chatbox = document.getElementById('chatbox'),
+  chatInput = document.getElementById('chat-input'),
+  chatSamp = document.getElementById('chat-samp')
+
+let incomingMessages = [],
+  scheduled
+
+let prefix = ''
+if (SUBDOMAIN !== 'localhost') {
+  prefix = SUBDOMAIN + '_'
+}
+const protocol = prefix + window.location.pathname.replace(/\//g, '_')
+const ws = new WebSocket('wss://chat.shoogle.net/',  protocol)
+let numberMessages = 0;
+
+ws.onmessage = message => {
+  incomingMessages.push(createMessage(message.data))
+
+  if (!scheduled) {
+    scheduled = true
+    window.requestAnimationFrame(() => {
+      const frag = document.createDocumentFragment()
+      for (let i = 0, len = incomingMessages.length; i < len; i++) {
+        frag.appendChild(incomingMessages[i])
+      }
+      appendToList(frag)
+      updateNumberMessages(incomingMessages.length)
+      incomingMessages.length = 0
+      scheduled = false
+    })
+  }
+}
+
+document.getElementById('chat-btn').onclick = () => sendTextMessage()
+
+document.getElementById('chat-form').onkeypress = event => {
+  if (event.keyCode === 13) {
+    event.preventDefault()
+    sendTextMessage()
+  }
 }

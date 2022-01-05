3b2f46f3415f11053e56f11f4ee5d9e48a750c0d
midzer <midzer@gmail.com>
2022-01-05 19:15:44 +0100

    fix slugo special char

--- assets/js/slugo.js
+++ assets/js/slugo.js
@@ -7,10 +7,10 @@
   function slugo(input) {
     return input // Remove html tags
     .replace(/<(?:.|\n)*?>/gm, '') // Remove special characters
-    .replace(/[!\"#$%'\(\)\*\+,\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '') // eslint-disable-line no-useless-escape
+    .replace(/[!\"#$%\(\)\*\+,\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '') // eslint-disable-line no-useless-escape
     // Replace dots and spaces with a short dash
     .replace(/(\s|\.)/g, '-') // Replace long dash with a single dash
-    .replace(/—/g, '-') // Make the whole thing lowercase
+    .replace(/[—']/g, '-') // Make the whole thing lowercase
     .toLowerCase() // Replace '&' with 'und'
     .replace(/[&]/g, 'und') // Replace Umlauts
     .replace(/[ÄäÖöÜü]/g, function (char) {
